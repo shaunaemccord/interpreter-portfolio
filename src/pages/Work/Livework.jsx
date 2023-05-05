661823fc80cfa19715ebddafc05a51cf9896b114
Tyler Bedard <tylerbedard126@gmail.com>
2023-05-05 17:29:03 -0400

    hopefully fixed pictures and added some text

--- src/pages/Work/Livework.jsx
+++ src/pages/Work/Livework.jsx
@@ -1,37 +1,38 @@
 const videos = [
     {
-        image: "/images/subject-matter-2023.png",
+        image: "/interpreter-portfolio/images/subject-matter-2023.png",
         name: "Subject Matter Presentation 2023",
         link: "https://docs.google.com/presentation/d/1ruEIsDjKAn5iOBB1ke9W9v6E5qNOGCn8/edit#slide=id.p1",
         description: "Subject Matter Presentation 2023",
-        stuff: "talk about it here"
+        stuff: "In Spring 2023 semester, my subject matter expert presentation on art painting featured an instructional lesson on how to paint a starry night scene. These presentations for Spring 2023 included roleplaying for the class. Two students took on the role of team interpreters, and three other students had headphones on with white noise to only rely on the interpretation. These unique roleplaying experience features for these presentations added some new dynamic challenges. I reflected on the knowledge that I gained from the previous presentation while planning out the details and execution for a live instructional painting lesson. I had to pay close attention to how long each section of the painting would take and I wanted to improve upon my engagement with the audience. I simplified the length of written text that I needed to present this time around. The only text that I required for the presentation I had featured on my PowerPoint and on one small piece of paper. I set a goal to challenge myself further with this presentation by selecting Vincent van Gogh’s The Starry Night for the painting I would depict with only classifiers and colors in American Sign Language. This painting was much more difficult to render, and I felt I was successful in my execution of a depiction for it. I felt more confident in this presentation and in teaching participants how to paint a starry night scene. I even remembered to breathe more during my presentation, and I reminded myself to be mindful not to speak too fast. Each participant was able to paint a starry night scene and keep their painting as a souvenir."
+
     },
     {
-        image: "/images/subject-matter-2022.png",
-        name: "Subject Matter Presentation 2023",
+        image: "/interpreter-portfolio/images/subject-matter-2022.png",
+        name: "Subject Matter Presentation 2022",
         link: "https://docs.google.com/presentation/d/1BwCUljeapUOKALZYktWB99WXRpYoxxHu/edit#slide=id.p1",
         description: "Subject Matter Presentation 2022",
-        stuff: "talk about it here"
+        stuff: "In Fall 2022 semester, I presented a subject matter expert presentation on art painting. It had been well over ten years since I was placed in a similar experience as a speaker to an audience. Though, art painting proved to be an excellent subject matter to present because of my previous background in studying art painting. Despite my comfort in the subject matter, I still felt very nervous to be the presenter for an audience of new classmates. I hid my face in my papers at times during this presentation and I spoke way too fast. My typed document for this presentation was over twelve pages long, which was way too long for this presentation. I learned more about depiction in this assignment and I appreciated the challenges I faced in creating a depiction of a painting with only classifiers and colors. The key terms list, definitions, synonyms, idioms, and quiz questions that I used for this assignment were also valuable in planning out a foundation for the following subject matter expert presentation."
     },
     {
-        image: "/images/translation-2022.png",
-        name: "Translation CC ASL Fall 2022",
+        image: "/interpreter-portfolio/images/translation-2022.png",
+        name: "Translation from written English to American Sign Language",
         link: "https://youtu.be/tV9awGgasnc",
         description: "Translation for fall semester 2022",
-        stuff: "talk about it here"
+        stuff: "I demonstrated appropriate role shifting, eye gaze, and accurate non-manual markers in this translation video. These features that I exhibited in this translation assist in creating a sense of engagement and connection for this translation. There are areas of opportunity for improvement regarding my sign production skills and how I handle glossed English words. I should also continue to improve upon my accuracy in visual depiction sign production. The ramp depiction is shown as too steep in this video and there are moments where my sign production is unclear or appears to reflect a different sign produced."
     },
     
     {
-        image: "/images/translation-2023.png",
-        name: "Translation 2023",
+        image: "/interpreter-portfolio/images/translation-2023.png",
+        name: "Translation from American Sign Language to written English.",
         link: null,
-        description: "A written translation from english to ASL Spring 2023",
-        stuff: "talk about it here",
+        description: "A written translation from ASL to english Spring 2023",
+        stuff: "In this written translation, I accurately translated the details that were shared about the client's e-book, and my written English is well matched for the tone expressed by the client. This reflects an understanding of both the content and the tone intended for the client’s message. I could improve upon how I begin English sentences and on the translation word choices for cultural mediation. An example of this is when I used the word “note” instead of video.",
         pdfUrl: "/pdf/spring-2023-translation.pdf"
     
     },
     {
-        image: "/images/consecutive-2023.png",
+        image: "/interpreter-portfolio/images/consecutive-2023.png",
         name: "How to change a Bike tire",
         link: "https://www.youtube.com/watch?v=G95_cXPYIPA",
         description: "A Consecutive Interpretation Work Bicycle ASL Spring 2023",
@@ -39,7 +40,7 @@ const videos = [
         pdfUrl: null
     },
     {
-        image: "/images/consecutive-2022.png",
+        image: "/interpreter-portfolio/images/consecutive-2022.png",
         name: "Cool and Unique",
         link: "https://www.youtube.com/watch?v=2bYJp13bcoM",
         description: "A Consecutive Interpretation Unique and Cool ASL Fall 2022",
@@ -47,7 +48,7 @@ const videos = [
         pdfUrl: null
     },
     {
-        image: "/images/simultaneous-2023.png",
+        image: "/interpreter-portfolio/images/simultaneous-2023.png",
         name: "Voting",
         link: "https://www.youtube.com/watch?v=sqdobQ9QySQ",
         description: "Simultaneous Interpretation Work Voting English Spring 2023",
@@ -55,7 +56,7 @@ const videos = [
         pdfUrl: null
     },
     {
-        image: "/images/simultaneous-spring-2023.png",
+        image: "/interpreter-portfolio/images/simultaneous-spring-2023.png",
         name: "Volunteerism",
         link: "https://www.youtube.com/watch?v=bpn33kA0sUs",
         description: "Simultaneous Interpretation Work Volunteerism English Spring 2023",
@@ -63,7 +64,7 @@ const videos = [
         pdfUrl: null
     },
     {
-        image: "/images/team-2023.png",
+        image: "/interpreter-portfolio/images/team-2023.png",
         name: "Team Interpreting",
         link: "https://www.youtube.com/watch?v=HCxTaI1N-dw",
         description: "Team Interpreting - Remain Model - Spring 2023",
@@ -71,7 +72,7 @@ const videos = [
         pdfUrl: null
     },
     {
-        image: "/images/ispeakjesus.png",
+        image: "/interpreter-portfolio/images/ispeakjesus.png",
         name: "I Speak Jesus ASL",
         link: "https://www.youtube.com/watch?v=2MYQ-uZfW50",
         description: "Internship Rock Church 2023  ",
@@ -79,7 +80,7 @@ const videos = [
         stuff: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue. Tempor orci dapibus ultrices in iaculis nunc. Elit sed vulputate mi sit amet mauris commodo. Sem nulla pharetra diam sit amet nisl. Amet risus nullam eget felis eget nunc lobortis mattis aliquam. Nibh ipsum consequat nisl vel pretium. Viverra adipiscing at in tellus integer feugiat scelerisque. Ultricies integer quis auctor elit sed. In massa tempor nec feugiat nisl pretium fusce id velit. Elementum curabitur vitae nunc sed velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Varius quam quisque id diam vel. In ante metus dictum at. Amet dictum sit amet justo donec enim diam vulputate. Euismod in pellentesque massa placerat. Auctor urna nunc id cursus metus aliquam eleifend mi in. Eu lobortis elementum nibh tellus molestie nunc non blandit. Duis at tellus at urna condimentum mattis pellentesque id."
 },
 {
-    image: "/images/fresh-wind.png",
+    image: "/interpreter-portfolio/images/fresh-wind.png",
     name: "Fresh Wind ASL",
     link: "https://www.youtube.com/watch?v=HX_t2U22sxM",
     description: "Internship Rock Church 2023",
